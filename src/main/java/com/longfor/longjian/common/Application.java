19a2c061a224e1050d1c47091abe92b6730217ca
李培帅_集团总部 <lipeishuai@www.lhwork.net>
2018-11-12 15:25:58 +0800

    first

---
+++ src/main/java/com/longfor/longjian/common/Application.java
@@ -0,0 +1,16 @@
+package com.longfor.longjian.common;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author lipeishuai
+ * @date 2018-11-10 10:45
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
