e35109047f5708111f943769f0d8afd77f7d3e38
a-yanglei <a-yanglei>
2019-01-17 18:14:57 +0800

    2019-01-17 杨磊-增加图片加批注功能

--- src/main/java/com/longfor/longjian/common/util/WaterMarkUtils.java
+++ src/main/java/com/longfor/longjian/common/util/WaterMarkUtils.java
@@ -1,10 +1,13 @@
 package com.longfor.longjian.common.util;
 
+import org.apache.commons.lang3.tuple.Pair;
+
 import javax.imageio.ImageIO;
 import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.FileOutputStream;
+import java.io.IOException;
 
 /**
  * @use 利用Java代码给图片加水印
@@ -23,7 +26,7 @@ public class WaterMarkUtils {
      */
     public static void addWaterMark(String srcImgPath, String tarImgPath, String waterMarkContent, Color markContentColor, Font font,int posx,int posy,int markWidth,int markHeight) {
 
-        try {
+        try(FileOutputStream outImgStream = new FileOutputStream(tarImgPath)) {
             // 读取原图片信息
             File srcImgFile = new File(srcImgPath);//得到文件
             Image srcImg = ImageIO.read(srcImgFile);//文件转化为图片
@@ -48,11 +51,11 @@ public class WaterMarkUtils {
             g.drawString(waterMarkContent, posx+2, posy+2);  //画出水印
             g.dispose();
             // 输出图片
-            FileOutputStream outImgStream = new FileOutputStream(tarImgPath);
+            //FileOutputStream outImgStream = new FileOutputStream(tarImgPath);
             ImageIO.write(bufImg, "png", outImgStream);
             System.out.println("添加水印完成");
             outImgStream.flush();
-            outImgStream.close();
+           // outImgStream.close();
 
         } catch (Exception e) {
             e.printStackTrace();
@@ -61,6 +64,14 @@ public class WaterMarkUtils {
     }
     public static int getWatermarkLength(String waterMarkContent, Graphics2D g) {
         return g.getFontMetrics(g.getFont()).charsWidth(waterMarkContent.toCharArray(), 0, waterMarkContent.length());
+    }
+
+    public static Pair<Integer,Integer> getImageInfo(String filePath) throws IOException {
+        File srcImgFile = new File(filePath);//得到文件
+        Image srcImg = ImageIO.read(srcImgFile);//文件转化为图片
+        int srcImgWidth = srcImg.getWidth(null);//获取图片的宽
+        int srcImgHeight = srcImg.getHeight(null);//获取图片的高
+        return Pair.of(srcImgWidth,srcImgHeight);
     }
     /*public static void main(String[] args) {
         Font font = new Font("微软雅黑", Font.PLAIN, 35);                     //水印字体
