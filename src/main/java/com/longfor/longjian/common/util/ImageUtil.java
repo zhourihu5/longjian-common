980cb1f131eed68c782fceaea11a4517ff6f22e4
zhouxingjia <zhouxingjia@longfor.com>
2019-01-18 11:31:27 +0800

    删除图片无用包

--- src/main/java/com/longfor/longjian/common/util/ImageUtil.java
+++ src/main/java/com/longfor/longjian/common/util/ImageUtil.java
@@ -1,12 +1,5 @@
 package com.longfor.longjian.common.util;
 
-import com.itextpdf.xmp.impl.Base64;
-import com.sun.image.codec.jpeg.JPEGCodec;
-import com.sun.image.codec.jpeg.JPEGEncodeParam;
-import com.sun.image.codec.jpeg.JPEGImageEncoder;
-import org.bouncycastle.util.encoders.UrlBase64Encoder;
-import sun.awt.SunHints;
-
 import javax.imageio.ImageIO;
 import java.awt.*;
 import java.awt.image.BufferedImage;
@@ -113,12 +106,12 @@ public class ImageUtil {
         System.out.println(a);
     }
 
-    public static void main(String[] args) throws Exception {
-//        resizeImage("E:/image/src.jpg", "E:/image/des.jpg", 100);//将图片压缩至100宽
-//        base64();
-        String srcImgPath = "E:/image/src.jpg"; //源图片地址
-        String tarImgPath = "E:/image/des.jpg"; //待存储的地址
-        String waterMarkContent = "张顺东-恒辉保温-海晶-项目经理 2019-01-31";  //水印内容
-        addWaterMark(srcImgPath, tarImgPath, waterMarkContent, 200);
-    }
+//    public static void main(String[] args) throws Exception {
+////        resizeImage("E:/image/src.jpg", "E:/image/des.jpg", 100);//将图片压缩至100宽
+////        base64();
+//        String srcImgPath = "E:/image/src.jpg"; //源图片地址
+//        String tarImgPath = "E:/image/des.jpg"; //待存储的地址
+//        String waterMarkContent = "张顺东-恒辉保温-海晶-项目经理 2019-01-31";  //水印内容
+//        addWaterMark(srcImgPath, tarImgPath, waterMarkContent, 200);
+//    }
 }
