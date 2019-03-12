bd974a4091357d1fc6f80b9507af55f8a7ea5778
a-wangxin1 <xin.wang@northking.net>
2019-03-12 10:58:03 +0800

    修改技术债

--- src/main/java/com/longfor/longjian/common/util/RSAUtil.java
+++ src/main/java/com/longfor/longjian/common/util/RSAUtil.java
@@ -8,21 +8,28 @@ import java.security.spec.PKCS8EncodedKeySpec;
 import java.security.spec.X509EncodedKeySpec;
 import javax.crypto.Cipher;
 
+import com.longfor.longjian.common.exception.CommonException;
+
 /** * @description RSA工具类 * @author bazhandao * @date 2018/12/26 18:40 * @since JDK1.8 */
 public class RSAUtil {
 
     /** * RSA加密的方法 * @author bazhandao * @date 2018-12-27 * @return rsa加密后转换成的base64加密字符串 */
-    public static String encrypt(String password, String publicKeyStr) throws Exception {
-        BASE64Decoder b64d = new BASE64Decoder();
-        byte[] keyByte = b64d.decodeBuffer(publicKeyStr);
-        X509EncodedKeySpec x509ek = new X509EncodedKeySpec(keyByte);
-        KeyFactory keyFactory = KeyFactory.getInstance("RSA");
-        PublicKey publicKey = keyFactory.generatePublic(x509ek);
-        Cipher cipher = Cipher.getInstance("RSA");
-        cipher.init(Cipher.ENCRYPT_MODE, publicKey);
-        byte[] sbt = password.getBytes();
-        byte[] epByte = cipher.doFinal(sbt);
-        BASE64Encoder base64Encoder = new BASE64Encoder();
-        return base64Encoder.encode(epByte);
+    public static String encrypt(String password, String publicKeyStr) throws CommonException {
+        try {
+            BASE64Decoder b64d = new BASE64Decoder();
+            byte[] keyByte = b64d.decodeBuffer(publicKeyStr);
+            X509EncodedKeySpec x509ek = new X509EncodedKeySpec(keyByte);
+            KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+            PublicKey publicKey = keyFactory.generatePublic(x509ek);
+            Cipher cipher = Cipher.getInstance("RSA");
+            cipher.init(Cipher.ENCRYPT_MODE, publicKey);
+            byte[] sbt = password.getBytes();
+            byte[] epByte = cipher.doFinal(sbt);
+            BASE64Encoder base64Encoder = new BASE64Encoder();
+            return base64Encoder.encode(epByte); 
+        } catch (Exception e) {
+           throw new CommonException(e);
+        }
+        
     }
 }
