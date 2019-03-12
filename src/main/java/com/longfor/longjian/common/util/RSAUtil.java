ea42982a730cc3315d8ccd5e422d78272e360da5
ds <572605334@qq.com>
2019-03-12 15:58:45 +0800

    修改 RSAUtil 异常

--- src/main/java/com/longfor/longjian/common/util/RSAUtil.java
+++ src/main/java/com/longfor/longjian/common/util/RSAUtil.java
@@ -1,20 +1,19 @@
 package com.longfor.longjian.common.util;
 
+import com.longfor.longjian.common.exception.LjBaseRuntimeException;
 import sun.misc.BASE64Decoder;
 import sun.misc.BASE64Encoder;
-import java.io.*;
-import java.security.*;
-import java.security.spec.PKCS8EncodedKeySpec;
-import java.security.spec.X509EncodedKeySpec;
-import javax.crypto.Cipher;
 
-import com.longfor.longjian.common.exception.CommonException;
+import javax.crypto.Cipher;
+import java.security.KeyFactory;
+import java.security.PublicKey;
+import java.security.spec.X509EncodedKeySpec;
 
 /** * @description RSA工具类 * @author bazhandao * @date 2018/12/26 18:40 * @since JDK1.8 */
 public class RSAUtil {
 
     /** * RSA加密的方法 * @author bazhandao * @date 2018-12-27 * @return rsa加密后转换成的base64加密字符串 */
-    public static String encrypt(String password, String publicKeyStr) throws CommonException {
+    public static String encrypt(String password, String publicKeyStr) throws LjBaseRuntimeException {
         try {
             BASE64Decoder b64d = new BASE64Decoder();
             byte[] keyByte = b64d.decodeBuffer(publicKeyStr);
@@ -28,7 +27,7 @@ public class RSAUtil {
             BASE64Encoder base64Encoder = new BASE64Encoder();
             return base64Encoder.encode(epByte); 
         } catch (Exception e) {
-           throw new CommonException(e);
+           throw new LjBaseRuntimeException(-9999,e+"");
         }
         
     }
